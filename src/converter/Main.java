8b8c0d9018b26a7f6ef2e64f96052b9d3dc60b5c
Kserhi <movie.to.48@gmail.com>
2024-01-04 17:54:37 +0200

    Add конвертація миль в кілометри

--- src/converter/Main.java
+++ src/converter/Main.java
@@ -1,4 +1,14 @@
 package converter;
 
 public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Одна миля це "+mileToKm(1)+" кілометер");
+
+    }
+
+    public static double mileToKm(double mile){
+        return mile*1.6;
+    }
+
 }
