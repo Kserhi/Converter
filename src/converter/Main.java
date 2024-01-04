2a562e19629c7dca2cb262a9341ca6147d2984b0
Kserhi <movie.to.48@gmail.com>
2024-01-04 18:00:31 +0200

    Add конвертація колометрів в милі

--- src/converter/Main.java
+++ src/converter/Main.java
@@ -1,4 +1,11 @@
 package converter;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Один кілометер це "+kmToMile(1)+" миля");
+    }
+
+    public static double kmToMile(double km){
+        return km/1.6;
+    }
 }
